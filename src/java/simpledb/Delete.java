d6545705c750ac656ca79cdbc133cb930d11e7b1
Timmy <784797673@qq.com>
2020-03-28 14:29:11 +0800

    DeleteTest pass(lab2 exercise4 finish). 20200328 1428

--- src/java/simpledb/Delete.java
+++ src/java/simpledb/Delete.java
@@ -10,6 +10,12 @@ public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private final TransactionId t;
+    private OpIterator child;
+
+    private TupleDesc td;
+    private int cnt_called;
+    private boolean is_called;
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
@@ -21,23 +27,37 @@ public class Delete extends Operator {
      */
     public Delete(TransactionId t, OpIterator child) {
         // some code goes here
+        this.t=t;
+        this.child=child;
+        this.td=new TupleDesc(new Type[]{Type.INT_TYPE},new String[]{null});
+        this.cnt_called=0;
+        this.is_called=false;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return td;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.open();
+        super.open();
+        this.cnt_called=0;
+        this.is_called=false;
     }
 
     public void close() {
         // some code goes here
+        this.is_called=true;
+        this.cnt_called=0;
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        child.rewind();
     }
 
     /**
@@ -51,18 +71,35 @@ public class Delete extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        if(is_called) return null;
+        is_called=true;
+
+        while(child.hasNext()){
+            Tuple temp_tuple=child.next();
+            try {
+                Database.getBufferPool().deleteTuple(t, temp_tuple);
+                cnt_called++;
+            }
+            catch(IOException ioe_exception){
+                ioe_exception.printStackTrace();
+                break;
+            }
+        }
+        Tuple ret_tuple=new Tuple(td);
+        ret_tuple.setField(0,new IntField(cnt_called));
+        return ret_tuple;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[]{child};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        child=children[0];
     }
 
 }
